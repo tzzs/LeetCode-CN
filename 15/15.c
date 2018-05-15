4ed0ee8be1a38ee4022621c1259f5c07a81ecd08
tzz <1789803837@qq.com>
2018-05-15 18:35:00 +0800

    删除多余文件

--- 15/15.c
+++ 15/15.c
@@ -1,4 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int **threeSum(int *nums, int numsSize, int *returnSize)
+{
+    int **Sum;
+    
+}
 
 int main(int argc, char const *argv[])
 {
