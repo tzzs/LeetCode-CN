c57e60b9ee1f97dbf405431173f17cd7c46c026d
tzzs <1789803837@qq.com>
2019-04-08 18:11:33 +0800

    AC ALL\998.validate-binary-search-tree\JAVA.java

---
+++ ALL/998.validate-binary-search-tree/JAVA.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    long max, min = Long.MIN_VALUE;
+    List<Integer> list = new ArrayList<>();
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        inorder(root);
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) >= list.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void inorder(TreeNode root) {
+        if (root != null) {
+            inorder(root.left);
+            list.add(root.val);
+            inorder(root.right);
+        }
+    }
+}
