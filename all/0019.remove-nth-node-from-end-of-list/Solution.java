8b1e9d3d72818657c886638b2234dc3a86edbaf4
TZZ <1789803837@qq.com>
2021-01-12 23:59:46 +0800

    ✔ 0019 删除链表的倒数第N个节点 use Java

---
+++ all/0019.remove-nth-node-from-end-of-list/Solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode nh = new ListNode(0, head);
+        ListNode l = nh, r = head;
+
+        for (int i = 0; i < n; i++) {
+            r = r.next;
+        }
+
+        while (r != null) {
+            l = l.next;
+            r = r.next;
+        }
+
+        l.next = l.next.next;
+        return nh.next;
+    }
+}
