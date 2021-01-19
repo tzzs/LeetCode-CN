6be80514e16c239b765d99c9d673dee924c543f7
TZZ <1789803837@qq.com>
2021-01-19 19:42:08 +0800

    🛠更新脚本，增加数字识别功能

--- newAc.bat
+++ newAc.bat
@@ -3,10 +3,16 @@ cd all
 @REM dir
 
 @REM get id
-set /p id=请输入ID: 
+set /p a=请输入ID: 
 
-Set "id=0000%id%"
-Set "id=%id:~-4,4%"
+set /a b=^%a%*1
+if %b% equ %a% (
+    Set "a=0000%a%"
+    Set "a=%a:~-4,4%
+)
+
+@REM Set "id=0000%id%"
+@REM Set "id=%id:~-4,4%"
 
 @REM get chinese name
 set /p chinese_name=请输入中文题目(chinese_name): 
@@ -14,7 +20,7 @@ set /p chinese_name=请输入中文题目(chinese_name):
 @REM get english_name
 set /p english_name=请输入英文题目(english_name): 
 
-set floder=%id%.%english_name%
+set floder=%a%.%english_name%
 
 md %floder%
 
@@ -33,9 +39,9 @@ echo=> Solution.%LAN_LOWER%
 
 
 @REM 生成markdown内容
-echo "| %id% | [%chinese_name%](./all/%floder%) | [%english_name%](./all/%floder%) |  | %lan% |"
+echo "| %a% | [%chinese_name%](./all/%floder%) | [%english_name%](./all/%floder%) |  | %lan% |"
 
 
 @REM 生成commit内容
 
-echo "%id% %chinese_name% use %lan% "
+echo "%a% %chinese_name% use %lan% "
