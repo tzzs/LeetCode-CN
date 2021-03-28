2fda8eb9dee6e310a6c1c767edc1bfa9a3588b14
TZZ <1789803837@qq.com>
2021-03-29 00:32:24 +0800

    ✔ lcof24 反转链表 AC use java

---
+++ all/lcof24.fan-zhuan-lian-biao-lcof/Solution.java
@@ -0,0 +1,24 @@
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+
+        while (head != null) {
+            ListNode tmp = new ListNode(head.val);
+            tmp.next = newHead;
+            newHead = tmp;
+
+            head = head.next;
+        }
+
+        return newHead;
+    }
+}
