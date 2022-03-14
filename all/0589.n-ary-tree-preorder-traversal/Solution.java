1b1fcb96b0f9038a97ed2c1cea9f768f41994736
TZZ <1789803837@qq.com>
2022-03-14 23:38:51 +0800

    ✔ 0589 N 叉树的前序遍历 AC use java

---
+++ all/0589.n-ary-tree-preorder-traversal/Solution.java
@@ -0,0 +1,31 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> list = new ArrayList<Integer>();
+        if (root == null)
+            return list;
+        list.add(root.val);
+        for (Node nd : root.children) {
+            list.addAll(preorder(nd));
+        }
+        return list;
+    }
+}
