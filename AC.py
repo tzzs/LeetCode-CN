ea35554939846ed84b8cb12d1b9ab6a0f22866bc
TZZ <1789803837@qq.com>
2022-04-02 01:16:17 +0800

    update ac script

--- AC.py
+++ AC.py
@@ -11,7 +11,7 @@ from time import gmtime, strftime
 if __name__ == '__main__':
     num = input('请输入ID:')
     if num.isdigit():
-        num = '0'*(4-len(str(num))) + num
+        num = '0' * (4 - len(str(num))) + num
 
     chinese_name = input('请输入中文题目(chinese_name):')
     englist_name = input('请输入英文题目(english_name):')
@@ -19,11 +19,11 @@ if __name__ == '__main__':
     lan = lan.lower()
     DIFFICULTY = input('请输入难度:')
 
-    if DIFFICULTY == '简单':
+    if DIFFICULTY == '简单' or DIFFICULTY == '1':
         DIFFICULTY = 'easy'
-    elif DIFFICULTY == '中等':
+    elif DIFFICULTY == '中等' or DIFFICULTY == '2':
         DIFFICULTY = 'medium'
-    elif DIFFICULTY == '困难':
+    elif DIFFICULTY == '困难' or DIFFICULTY == '3':
         DIFFICULTY = 'hard'
 
     DIFFICULTY = DIFFICULTY.lower()
@@ -41,12 +41,15 @@ if __name__ == '__main__':
     if not os.path.exists(path):
         os.makedirs(path)
 
+    FILE_PATH = ""
     if not os.path.exists(path + '/' + file_name):
-        file = open(path + '/' + file_name, 'w')
+        FILE_PATH = path + '/' + file_name
+        file = open(FILE_PATH, 'w')
         file.close()
     else:
-        file = open(path + '/' + 'Solution_' +
-                    strftime("%Y-%m-%d-%H-%M-%S", gmtime())+'.' + FILE_TYPE, 'w')
+        FILE_PATH = path + '/' + 'Solution_' + \
+            strftime("%Y-%m-%d-%H-%M-%S", gmtime())+'.' + FILE_TYPE
+        file = open(FILE_PATH, 'w')
         file.close()
 
     dir_path = './all/' + num + '.' + englist_name
@@ -58,6 +61,7 @@ if __name__ == '__main__':
     commit_msg = '✔ ' + num + ' ' + chinese_name + ' AC use ' + lan
     print(commit_msg)
 
+    # update problems infomation to README.md
     with open("README.md", "r", encoding='utf-8') as f:
         content = f.readlines()
 
@@ -76,7 +80,13 @@ if __name__ == '__main__':
     with open("README.md", "w", encoding='utf-8') as f:
         f.writelines(content)
 
+    # open file in current vscode window
+    os.system('code -r ' + FILE_PATH)
+
+    # wait for write code to the file
     os.system('pause')
+
+    # commit ac code and push to github
     os.system('git add ' + dir_path)
     os.system('git add README.md')
     os.system('git commit -m "' + commit_msg + '"')
